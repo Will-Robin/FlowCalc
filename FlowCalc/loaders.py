4fa2194b638b3b56a14e504f27e80397e3ab6cc3
Will <werobinson6@gmail.com>
2022-02-10 11:56:05 +0100

    create function for intialising flow experiment from file

---
+++ FlowCalc/loaders.py
@@ -0,0 +1,35 @@
+from FlowCalc import Classes
+
+def flow_experiment_from_file(filename):
+    '''
+    Initialise a flow experiment from a formatted file.
+
+    Parameters
+    ----------
+    filename: str
+        Name of a formatted configuration file.
+
+    Returns
+    -------
+    experiment: Classes.FlowExperiment
+    '''
+
+    with open(filename, 'r') as file:
+        text = file.read()
+
+    lines = text.split('\n')
+
+    entries = []
+    for l in lines:
+        entry = l.split(',')
+        entries[entry[0]] = entry[1]
+
+    experiment = Classes.FlowExperiment(entries['Exp_code'])
+
+    reactor_vol = entries['Reactor_volume']
+    reactor_unit = entries['Reactor_volume_unit']
+
+    experiment.reactor_volume = float(reactor_vol)
+    experiment.reactor_volume_unit = float(reactor_unit)
+
+    return experiment
