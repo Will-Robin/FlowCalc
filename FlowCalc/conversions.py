bfea7dd34f43fe1a2123eaee60d14feb57f17c35
Will <werobinson6@gmail.com>
2022-02-10 11:14:24 +0100

    update conversions dict

--- FlowCalc/conversions.py
+++ FlowCalc/conversions.py
@@ -2,6 +2,7 @@ import numpy as np
 # A dictionary of lambda functions used to convert flow
 # profile units into SI units.
 SI_conversions = {
+                    "L/s"    : (lambda x: x, "L/s"),
                     "µl/h"   : (lambda x: x/(360*1e6), "L/s"),
                     "µl/min" : (lambda x: x/(60*1e6), "L/s"),
                     "µl/s"   : (lambda x: x/10e6, "L/s"),
@@ -13,9 +14,16 @@ SI_conversions = {
                     "l/h"    : (lambda x: x/360, "L/s"),
                     "l/min"  : (lambda x: x/60, "L/s"),
                     "l/s"    : (lambda x: x, "L/s"),
+
+                    "s"      : (lambda x: x, "s"),
                     "h"      : (lambda x: x*60, "s"),
+
+                    "rad"    : (lambda x: x, "rad"),
                     "degrees": (lambda x: x*np.pi/180, "rad"),
-                    "uL/h"   : (lambda x: x*(1e-6)/(60*60), "L/s"),
+
+                    "M"      : (lambda x: x, "M"),
+                    "mM"     : (lambda x: x*1e-3, "M"),
+                    "μM"     : (lambda x: x*1e-6, "M"),
                     "uM"     : (lambda x: x*1e-6, "M"),
-                    "mM"     : (lambda x: x*1e-3, "M")
+                    "nM"     : (lambda x: x*1e-9, "M")
                  }
