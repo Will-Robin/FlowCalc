1fdaeeacf93bf74e1d3378053b427fdeb8658f49
Will <werobinson6@gmail.com>
2022-03-08 23:05:28 +0100

    update examples

--- Scripts/Example.py
+++ Scripts/Example.py
@@ -38,3 +38,5 @@ experiment.write_flow_profile('ExampleOutput')
 
 conditions_filename = f"ExampleOutput/{experiment.name}_conditions.csv"
 experiment.write_conditions_file(conditions_filename)
+experiment.write_toml("test.toml")
+
