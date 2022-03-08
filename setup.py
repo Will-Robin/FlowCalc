8f7cd5289cfefbee1d8980bc4161e5a453afe81a
Will <werobinson6@gmail.com>
2022-03-08 23:04:13 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, version
+
+setup(
+    name="FlowCalc",
+    version="0.0",
+    author="William E. Robinson",
+    packages = ["FlowCalc"],
+    install_requires=[
+        "numpy >= 1.21.5",
+    ],
+)
